09a4822b46b121464036a07060400640c52a4c8c
gokel166 <okellogeorge40@yahoo.com>
2017-12-01 11:51:02 -0600

    new commit

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux'
+
+function receivePosts (state = {} , action) {
+    switch(action.type){
+        case GET_POSTS:
+            return { ...state, ...action.posts }
+        default:
+            return state
+    }
+}
+
+function sorting (state = {} , action) {
+    switch(action.type){
+        case SET_SORTING:
+            return { ...state, ...action.sorter }
+        default:
+            return state
+    }
+}
